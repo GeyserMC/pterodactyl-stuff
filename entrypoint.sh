e2d029412c31c1bf0b38e19387aa74ee2bc7fb98
Greaper88 <61922763+Greaper88@users.noreply.github.com>
2020-12-30 14:44:49 -0600

    Update entrypoint.sh (#12)

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,13 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 if [ "${AUTO_UPDATE}" == "1" ]; then
 	echo "Checking for updates..."
 
-	LATEST_HASH=`curl -s https://ci.nukkitx.com/job/GeyserMC/job/GeyserConnect/job/${UPDATE_BRANCH}/lastSuccessfulBuild/api/xml?xpath=//lastBuiltRevision/SHA1 | sed 's/.*>\(.*\)<.*/\1/'`
+	LATEST_HASH=`curl -s https://ci.opencollab.dev/job/GeyserMC/job/GeyserConnect/job/${UPDATE_BRANCH}/lastSuccessfulBuild/api/xml?xpath=//lastBuiltRevision/SHA1 | sed 's/.*>\(.*\)<.*/\1/'`
 	CURRENT_HASH=`cat .currenthash 2>/dev/null`
 
 	if [ "$LATEST_HASH" != "$CURRENT_HASH" ]; then
 		echo "Update available!"
 		echo "Updating from '$CURRENT_HASH' -> '$LATEST_HASH'"
-		curl -s -o ${SERVER_JARFILE} https://ci.nukkitx.com/job/GeyserMC/job/GeyserConnect/job/${UPDATE_BRANCH}/lastSuccessfulBuild/artifact/target/GeyserConnect.jar
+		curl -s -o ${SERVER_JARFILE} https://ci.opencollab.devjob/GeyserMC/job/GeyserConnect/job/${UPDATE_BRANCH}/lastSuccessfulBuild/artifact/target/GeyserConnect.jar
 
 		echo "$LATEST_HASH" > ".currenthash"
 		echo "Updated!"
